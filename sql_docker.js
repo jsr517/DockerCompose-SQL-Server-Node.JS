3d4fce0a1de894e0c7b06ec6acc04261dbd1862d
Jarnail Singh <96626033+jsr517@users.noreply.github.com>
2023-01-13 13:04:18 -0800

    Create sql_docker.js
    to connect to sql database and fetch data

---
+++ sql_docker.js
@@ -0,0 +1,21 @@
+const sql = require('mssql');
+
+const config = {
+    server: 'localhost',
+    port: 1433,
+    user: 'sa',
+    password: 'your_password',
+    database: 'your_database',
+};
+
+async function runQuery(query) {
+    try {
+        const pool = await sql.connect(config);
+        const result = await pool.request().query(query);
+        console.log(result);
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+runQuery("SELECT * FROM your_table");
